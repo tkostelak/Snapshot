e5715b9ea5559d39b821e81c51757ad040ea1bd9
tyler kostelak <tkostelak@gmail.com>
2018-03-30 15:45:39 -0700

    routes file adds routing to project and will eventually allow capability to view specific images.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router'
 
 import { ImageFilterPipe} from './shared/filter.pipe'
 import { ImageService } from './shared/image.service';
@@ -8,6 +9,7 @@ import { AppComponent } from './app.component';
 import { NavbarComponent} from './navbar/navbar.component';
 import { GalleryComponent } from './gallery/gallery.component';
 import { ImageComponent } from './image/image.component';
+import { appRoutes } from '../routes';
 
 @NgModule({
   declarations: [
@@ -20,8 +22,9 @@ import { ImageComponent } from './image/image.component';
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [ImageService ImageFilterPipe],
+  providers: [ImageService, ImageFilterPipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
