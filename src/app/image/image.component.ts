fc9b2f0b33069950718d61ae034fa886c43243ba
tyler kostelak <tkostelak@gmail.com>
2018-03-30 16:11:55 -0700

    ngOnInit function in image component adds ability to take snapshot of image id that's retrieved in get image function and an image container is dynamically created in HTMl with a link and when user clicks it, they are routed to a new page corresponding to image id.

--- src/app/image/image.component.ts
+++ src/app/image/image.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ImageService } from '../shared/image.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-image',
   templateUrl: './image.component.html',
   styleUrls: ['./image.component.css']
 })
-export class ImageComponent {
+export class ImageComponent implements OnInit {
 
-  constructor(private imageService: ImageService) { }
+  image: any
 
+  constructor(private imageService: ImageService, private route: ActivatedRoute) { }
 
+  ngOnInit(){
+    this.image = this.imageService.getImage(
+      +this.route.snapshot.params['id']
+    )
+  }
 }
