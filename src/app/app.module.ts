7c1d8e77aca537143a77ae7eff16bb04258169dc
tyler kostelak <tkostelak@gmail.com>
2018-03-30 10:54:02 -0700

    removal of old version of bootstrap fixes github security warning and navbar component adds beginnings to create a navbar in the project.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent} from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
