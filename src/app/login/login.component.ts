448204072524b2612ef37822d4f00fcc273e2056
tyler kostelak <tkostelak@gmail.com>
2018-04-08 14:06:07 -0700

    login component login method adds login method that utilizes a promise to direct successful email and password credentials to gallery, and if those conditions are not met the user should receive an error message.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,18 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../services/authentication.service';
 import { Routing } from '../app.routing';
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  email: string;
+  password: string;
+  errorMsg: "Sorry, but the credentials you provided are incorrect. Please try again.";
 
-  ngOnInit() {
+  constructor(private authService: AuthenticationService, private router: Router) { }
+
+  signIn() {
+    this.authService.login({ email: this.email, password: this.password})
+      .then(resolve => this.router.navigate(['gallery']))
+      .catch(error => this.errorMsg = error.message);      
   }
 
 }
